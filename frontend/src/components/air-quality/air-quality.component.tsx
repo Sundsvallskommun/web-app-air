6a04a91df047cceec30cca38a519ef8d791f2e5a
Alice Fagerberg <alice.fagerberg@sundsvall.se>
2025-08-20 17:35:03 +0200

    build base for graph

--- frontend/src/components/air-quality/air-quality.component.tsx
+++ frontend/src/components/air-quality/air-quality.component.tsx
@@ -1,9 +1,34 @@
 import { useAirStore } from '@services/air-service/air-service';
 import { Spinner } from '@sk-web-gui/react';
+import { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer } from 'recharts';
+import dayjs from 'dayjs';
+import { PollutantType } from '@interfaces/pollutant/pollutant';
+
+interface AirQualityGraph {
+  [key: string]: string | number | [] | object | null | undefined;
+}
 
 export default function AirQualityComponent() {
   const airQuality = useAirStore((state) => state.airQuality);
   const airQualityIsLoading = useAirStore((state) => state.airQualityIsLoading);
+  const [graphData, setGraphData] = useState<AirQualityGraph[]>();
+
+  useEffect(() => {
+    if (airQuality && airQuality.pollutants && airQuality.pollutants.length > 0) {
+      const airArray: AirQualityGraph[] = [];
+      airQuality.pollutants.forEach((pollutant) => {
+        pollutant.values.forEach((v) => {
+          airArray.push({
+            name: pollutant.name,
+            observedAt: dayjs(v.observedAt).format('DD/MM/YY'),
+            [pollutant.name]: v.value,
+          });
+        });
+      });
+      setGraphData(airArray);
+    }
+  }, [airQuality]);
 
   console.log(airQuality);
   return (
@@ -14,7 +39,36 @@ export default function AirQualityComponent() {
       <div>
         {airQualityIsLoading ?
           <Spinner size={4} />
-        : <span>Klar</span>}
+        : graphData && (
+            <LineChart width={1020} height={300} data={graphData}>
+              <CartesianGrid />
+
+              <Line
+                dataKey={PollutantType.AtmosphericPressure}
+                stroke="purple"
+                name={PollutantType.AtmosphericPressure}
+              />
+              <Line dataKey={PollutantType.RelativeHumidity} stroke="green" name={PollutantType.RelativeHumidity} />
+              <Line dataKey={PollutantType.Temperature} stroke="pink" name={PollutantType.Temperature} />
+              <Line
+                dataKey={PollutantType.TotalSuspendedParticulate}
+                stroke="grey"
+                name={PollutantType.TotalSuspendedParticulate}
+              />
+              <Line dataKey={PollutantType.PM1} stroke="lightblue" name={PollutantType.PM1} />
+              <Line dataKey={PollutantType.PM4} stroke="yellow" name={PollutantType.PM4} />
+              <Line dataKey={PollutantType.PM10} stroke="darkblue" name={PollutantType.PM10} />
+              <Line dataKey={PollutantType.PM25} stroke="cyan" name={PollutantType.PM25} />
+              <Line dataKey={PollutantType.NO} stroke="red" name={PollutantType.NO} />
+              <Line dataKey={PollutantType.NO2} stroke="orange" name={PollutantType.NO2} />
+              <Line dataKey={PollutantType.NOx} stroke="black" name={PollutantType.NOx} />
+
+              <XAxis dataKey="observedAt" />
+              <YAxis />
+              <Legend />
+            </LineChart>
+          )
+        }
       </div>
     </section>
   );
---
+++ frontend/src/interfaces/pollutant/pollutant.ts
@@ -0,0 +1,13 @@
+export enum PollutantType {
+  AtmosphericPressure = 'Lufttryck',
+  Temperature = 'Temperatur (C)',
+  RelativeHumidity = 'Relativ Luftfuktighet (%)',
+  PM1 = 'PM1',
+  PM4 = 'PM4',
+  PM10 = 'PM10',
+  PM25 = 'PM25',
+  TotalSuspendedParticulate = 'upphängda partiklar',
+  NO = 'NO',
+  NO2 = 'NO2',
+  NOx = 'NOx',
+}
